628a2b0e50bc59fc2135e069d926d2369f489b8b
Minhyoyo <nguyentrachminh@gmail.com>
2025-02-04 16:30:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, ArgoCD! Tôi là Minhyoyo, rất vui được làm quen với bạn"
+    return "Hello, ArgoCD! Tôi là Minhyoyo, rất vui được làm quen với bạn. Cộng hòa xã hội chủ nghĩa việt nam"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
